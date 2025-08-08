c4c00922456515bd9722af00c58c6c8afffe37c7
Bmondragon231 <157536480+Bmondragon231@users.noreply.github.com>
2025-08-07 22:43:09 -0600

    seeders de proveedor

--- database/seeders/ProveedorSeeder.php
+++ database/seeders/ProveedorSeeder.php
@@ -16,11 +16,46 @@ class ProveedorSeeder extends Seeder
         Proveedor::create([
             'rtn' => '08011999999',
             'nombre_proveedor' => 'Proveedor de Ejemplo',
-            'telefono' => '1234-5678',
+            'telefono' => '12345678',
             'numero_adjudicacion' => 'ADJ-001',
             'tipo_adjudicacion_id' => 1,
             'tipo_proveedor_id' => 1,
-            'creador_id' => 1
+            'creador_id' => 1, // Assuming user with ID 1 exists
+            
         ]);
+        Proveedor::create([
+            // Additional providers can be added here
+            'rtn' => '08011999998',
+            'nombre_proveedor' => 'Banasupro',
+            'telefono' => '87654321',
+            'numero_adjudicacion' => 'ADJ-002',
+            'tipo_adjudicacion_id' => 1,
+            'tipo_proveedor_id' => 1,
+            'creador_id' => 1, // Assuming user with ID 1 exists
+              
+        ]);
+        Proveedor::insert([
+            'rtn' => '08011999997',
+            'nombre_proveedor' => 'La Gotita',
+            'telefono' => '23456789',
+            'numero_adjudicacion' => 'ADJ-003',
+            'tipo_adjudicacion_id' => 2,
+            'tipo_proveedor_id' => 1,
+            'creador_id' => 1, // Assuming user with ID 1 exists
+
+             
+        ]);
+        Proveedor::create([
+            'rtn' => '08011999996',
+            'nombre_proveedor' => 'Tamales y Más',
+            'telefono' => '34567890',
+            'numero_adjudicacion' => 'ADJ-004',
+            'tipo_adjudicacion_id' => 2,
+            'tipo_proveedor_id' => 1,
+            'creador_id' => 1, // Assuming user with ID 1 exists
+             
+        ]);
+        
+       
     }
 }
